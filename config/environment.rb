2813c89405d23a9dbd0c20980264a14e066eb158
J. Singleton <jennifer.singleton@gmail.com>
2019-02-27 02:19:19 +0000

    added required environment files

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+require_relative '../lib/cli-controller'
+require_relative '../lib/location'
+require_relative '../lib/ordinance'
+require_relative '../lib/scraper'
